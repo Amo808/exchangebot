e1dfd44136f43f78c3c0d2b42b21ad3ad0e9cdaa
Amo808 <162590817+Amo808@users.noreply.github.com>
2025-03-18 17:15:50 +0600

    Added root endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from app.api import router
 
-app = FastAPI()
+app = FastAPI(
+    title="ExchangeBot",
+    docs_url="/docs",
+    redoc_url="/redoc"
+)
+
+app.include_router(router)  # Верни маршруты!
 
 @app.get("/")
 def read_root():
